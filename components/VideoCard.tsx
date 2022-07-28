abc8b9ea364afa779867b40ac09b50cec6301c98
allexib <ooomegasib@gmail.com>
2022-07-28 13:13:52 +0300

    videoCard, add profile img

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,17 +1,46 @@
-import React from "react";
-import { Video } from "../types";
+import React, { useEffect, useRef, useState } from "react";
 import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import { BsPlay } from "react-icons/bs";
+
+import { Video } from "./../types";
 
 interface IProps {
   post: Video;
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
-  return <div>VideoCard</div>;
+  return (
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded ">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href='/'>
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className=" rounded-full"
+                  src={post.postedBy.image}
+                  alt="user-profile"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
 
+/*
 // import React, { useEffect, useRef, useState } from 'react';
 // import { NextPage } from 'next';
 // import Image from 'next/image';
@@ -156,3 +185,4 @@ export default VideoCard;
 // };
 //
 // export default VideoCard;
+*/
