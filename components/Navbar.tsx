f31e4fc0f12e267643485d9d6fca93a3a4b85c6e
allexib <ooomegasib@gmail.com>
2022-07-28 17:37:05 +0300

    react-oauth/google npm

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,17 +3,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { AiOutlineLogout } from "react-icons/ai";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
-import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
+
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
-      <Link href='/'>
-        <div className='w-[200px] md:w-[230px] '>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[200px] md:w-[230px] ">
           <Image
             className="cursor-pointer"
             src={Logo}
@@ -22,6 +24,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
