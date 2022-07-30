6831c136f98a2b7422275166a4a89e44ed8142b2
allexib <ooomegasib@gmail.com>
2022-07-30 15:27:07 +0300

    like, comment

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,36 +1,20 @@
-import React from 'react';
+import React from "react";
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
 
 interface IProps {
-    text:string;
+  text: string;
 }
 
-const NoResults = ({text}:IProps) => {
-    return (
-        <div>
-            NoResults
-        </div>
-    );
+const NoResults = ({ text }: IProps) => {
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === "No comments yet" ? <BiCommentX /> : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResults;
-
-
-// import React from 'react';
-// import { MdOutlineVideocamOff } from 'react-icons/md';
-//
-// interface IProps {
-//   text: string;
-// }
-//
-// const NoResults = ({ text }: IProps) => {
-//   return (
-//     <div className='flex flex-col justify-center items-center h-full w-full'>
-//       <p className='text-8xl'>
-//         <MdOutlineVideocamOff />
-//       </p>
-//       <p className='text-2xl text-center'>{text}</p>
-//     </div>
-//   );
-// };
-//
-// export default NoResults;
