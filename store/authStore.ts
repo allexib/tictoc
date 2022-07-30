694db3b8ca55b732a285d31d854da31a32021952
allexib <ooomegasib@gmail.com>
2022-07-30 17:53:37 +0300

    fetch all users, comments

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,20 +2,20 @@ import create from 'zustand';
 import { persist } from 'zustand/middleware';
 import axios from 'axios';
 
-// import { BASE_URL } from '../utils';
+import { BASE_URL } from '../utils';
 
 const authStore = (set: any) => ({
     userProfile: null,
-    // allUsers: [],
+    allUsers: [],
 
     addUser: (user: any) => set({ userProfile: user }),
     removeUser: () => set({ userProfile: null }),
 
-    // fetchAllUsers: async () => {
-    //     const response = await axios.get(`${BASE_URL}/api/users`);
-    //
-    //     set({ allUsers: response.data });
-    // },
+    fetchAllUsers: async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`);
+
+        set({ allUsers: response.data });
+    },
 });
 
 const useAuthStore = create((
