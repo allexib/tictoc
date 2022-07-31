fee918e2b5467198ab7c6af8c445a4a4eeb003f5
allexib <ooomegasib@gmail.com>
2022-07-31 10:43:39 +0300

    ts ignore errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
